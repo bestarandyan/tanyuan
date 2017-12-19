8fc6042e90f000c3b1d1fbab21fd76ffd5a67ebb
刘星星 <bestar@lifang.com>
2017-12-19 10:52:11 +0800

    login request

--- app/src/main/java/com/tanyuan/app/findyuan/FindYuanFragment.java
+++ app/src/main/java/com/tanyuan/app/findyuan/FindYuanFragment.java
@@ -1,8 +1,11 @@
 package com.tanyuan.app.findyuan;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,24 +14,48 @@ import android.view.ViewGroup;
 import com.tanyuan.app.R;
 import com.tanyuan.app.request.CircleListRequest;
 import com.tanyuan.app.response.BallFriendCirlceResponse;
+import com.tanyuan.app.response.CircleModel;
 import com.tanyuan.app.utils.preference.UserPreferenceUtils;
 import com.tanyuan.network.interfaces.RequestInterface;
 import com.tanyuan.network.request.RequestManager;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 /**
  * Created by liuxingxing on 2017/11/15.
  */
 
-@EFragment(R.layout.fragment_findyuan)
 public class FindYuanFragment extends Fragment {
+    RecyclerView mListView;
+    View mView;
 
-    @AfterViews
-    void init(){
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_findyuan,null);
+        initView();
+        return mView;
+    }
+
+    private void initView(){
+        mListView = mView.findViewById(R.id.dongtaiList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        mListView.setLayoutManager(layoutManager);
+    }
+
+    private void setView(final ArrayList<CircleModel> models){
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mListView.setAdapter(new CircleAdapter(getActivity(),models));
+            }
+        });
 
     }
 
@@ -52,6 +79,9 @@ public class FindYuanFragment extends Fragment {
                     @Override
                     public void onReceivedData(BallFriendCirlceResponse response) {
                         Log.e("获取动态返回结果 =====" ,response.getMessage());
+                        if (response!=null && response.succeeded()) {
+                            setView(response.data);
+                        }
                     }
 
                     @Override
